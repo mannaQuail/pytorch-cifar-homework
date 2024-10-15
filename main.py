d8e29794284777cb2975dfe56332d337bf1bf5d4
mannaQuail <injesus1010@gmail.com>
2024-10-15 17:10:26 +0900

    set epoch 100

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ def test(epoch):
 
 
 if __name__ == '__main__':
-    for epoch in range(start_epoch, start_epoch+200):
+    for epoch in range(start_epoch, start_epoch+100):
         train(epoch)
         test(epoch)
         scheduler.step()
